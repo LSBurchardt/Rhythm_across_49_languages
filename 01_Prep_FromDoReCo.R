6e3f3bf0cd6fce494e5cfa3775be809a676b2608
LuPaschen <LuPaschen@users.noreply.github.com>
2024-03-14 12:40:34 +0100

    Update 01_Prep_FromDoReCo.R
    Fixed a bug introduced by merging metadata too early

--- 01_Prep_FromDoReCo.R
+++ 01_Prep_FromDoReCo.R
@@ -15,33 +15,39 @@ wd_csv_data$speaker <- paste(wd_csv_data$lang,"_",wd_csv_data$speaker,sep="")
 # Calculate duration of individual words
 wd_csv_data <- wd_csv_data %>% mutate(duration = round(end - start, 3))
 
-# Read metadata CSV and merge dataframes
-metadata <- read_csv("csvs/metadata_merged_1_3.csv")
-metadata$file <- paste("doreco_",metadata$glottocode,"_",metadata$filename,sep="")
-df_merged <- merge(wd_csv_data,metadata,by="file")
-
-# Remove non-core speakers, i.e. data without proper time alignment for words and pauses
-df_clean <- subset(df_merged, (speaker %in% c(spk_code_a, spk_code_b)))
-
-# Remove conversational and stimulus-based data
-df_clean <- subset(df_clean, !(genre %in% c("conversation", "stimulus retelling")))
-
 # Identify IO units, calculate duration, check for labels, assign corpus-wide running integer
-io_data <- df_clean %>%
-  group_by(lang, file, speaker) %>%
+io_data <- wd_csv_data %>%
+  group_by(file, speaker) %>%
   mutate(is_sequence_start = (ph != "<p:>" & lag(ph, default = first(ph)) == "<p:>") | row_number() == 1,
          io_unit = cumsum(is_sequence_start)) %>%
-  group_by(lang, file, speaker, io_unit) %>%
+  group_by(file, speaker, io_unit) %>%
   filter(!(ph == "<p:>" & io_unit == 1)) %>%
   summarise(start_time = first(start),
             end_time = last(end),
             duration = sum(duration),
             only_labels = !(any(!str_starts(wd[wd != "<p:>"], '<<'))),
             .groups = 'drop') %>%
-  group_by(lang, file, speaker) %>%
+  group_by(file, speaker) %>%
   filter(io_unit != max(io_unit)) %>%
   ungroup() %>%
-  mutate(io_unit = row_number())  
-  
+  mutate(io_unit = row_number())
+
+# Read metadata CSV and merge dataframes
+metadata <- read.csv("csvs/metadata_merged_1_3.csv", na.strings = "na")
+metadata$file <- paste("doreco_",metadata$glottocode,"_",metadata$filename,sep="")
+io_merged <- merge(io_data,metadata,by="file")
+
+# Remove non-core speakers, i.e. data without proper time alignment for words and pauses
+io_clean <- subset(io_merged, (speaker %in% c(spk_code_a, spk_code_b)))
+
+# Remove conversational and stimulus-based data
+io_clean <- subset(io_clean, !(genre %in% c("conversation", "stimulus retelling")))
+io_clean <- subset(io_clean, is.na(spk_code_b))
+
+# Cleaning up
+io_clean <- subset(io_clean, select = -c(filename,rec_date,rec_date_c,genre_stim,extended,gloss,transl,sound_quality,background_noise,word_tokens,spk_code_a,spk_age_c_a,spk_code_b,spk_age_b,spk_age_c_b,spk_sex_b))
+lookup <- c(speaker_sex = "spk_sex_a", speaker_age = "spk_age_a")
+io_final <- rename(io_clean, all_of(lookup))  
+
 # Write to a new CSV file
-write_csv(io_data, "DoReCo_1_3_IO.csv") # This creates a new CSV file with the processed data
+write_csv(io_final, "DoReCo_1_3_IO.csv") # This creates a new CSV file with the processed data
