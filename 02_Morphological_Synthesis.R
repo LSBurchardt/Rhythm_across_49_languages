7dfdf3bf4a24e77c8ef31bb1d5b119ad16c5b1ed
LuPaschen <LuPaschen@users.noreply.github.com>
2024-08-29 17:51:01 +0200

    Update 02_Morphological_Synthesis.R
    add filter for next_to_wip

--- 02_Morphological_Synthesis.R
+++ 02_Morphological_Synthesis.R
@@ -14,14 +14,16 @@ synthesis_data <- wd_csv_data %>%
   select(lang, core_extended, wd, mb, mb_ID, gl) %>%
   filter(!is.na(gl)) %>%
   filter(core_extended == "core") %>%
+  mutate(next_to_wip = ifelse(
+    lead(wd) == "<<wip>>" | lag(wd) == "<<wip>>", "yes", "no")) %>%
+  filter(next_to_wip == "no") %>%
   filter(wd != "<p:>") %>%
   filter(!str_starts(wd, '<<')) %>%
   distinct(lang, wd, mb, .keep_all = T) %>%
   mutate(morph_count = str_count(mb_ID, "m")) %>%
   group_by(lang) %>%
   mutate(synthesis = round(mean(morph_count), 3)) %>%
-  summarise(synthesis = unique(synthesis), .groups = 'drop') %>%
-  ungroup()
+  summarise(synthesis = unique(synthesis), .groups = 'drop')
 
 # Write to a new CSV file
 write_csv(synthesis_data, "DoReCo_1_3_core_synthesis.csv")
